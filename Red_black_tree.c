7177a7eb0a149282aac5b887f7c593d1bed98035
fede-da <63853572+fede-da@users.noreply.github.com>
2021-04-28 12:04:06 +0200

    Add files via upload

---
+++ Red_black_tree.c
@@ -0,0 +1,58 @@
+//
+//  Red_black_tree.c
+//
+//  Created by Federico D'Armini on 25/11/2019.
+//
+
+#include "tmp.h"
+#include <string.h>
+#include <ctype.h>
+
+mynode* red_node_create(mynode*node, int key, int val);
+
+mynode* black_node_create(mynode*node, int key, int val);
+
+
+void tree_print(mynode*node);
+
+
+void insert ( mynode*node,mynode*root,int val,int key);
+
+void node_free (mynode* node);
+
+void tree_free (mynode* root);
+
+void welcome();
+
+int isInt (char* string);
+
+int getInput(char* string, size_t string_max); // add parameter to get buffer size
+
+
+
+int main (){
+    int key=1;
+    int height=1;
+    char input[12];
+    
+    welcome();
+
+    mynode*root=NULL;
+    getInput(input,12);
+    root=black_node_create(NULL,0,0);
+    root->parent=NULL;
+    root->right=black_node_create(root,key++,atoi(input));
+    root->right->parent=root;
+    printf("Node created!\n");
+    tree_print(root->right);
+    printf("Insert a number : \n");
+    while (memcmp("\n",input,1)!=0 )
+    {
+        getInput(input,12);
+        insert(root->right,root->right,atoi(input),key++);
+        printf("Node created!\n");
+        tree_print(root->right);
+        printf("Do you want to continue? Yes -> Insert a number, ENTER -> stop\n");
+    }
+    return 0;
+}
